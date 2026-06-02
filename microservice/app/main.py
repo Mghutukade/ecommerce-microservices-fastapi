baa977f1b09f55a8ef3186499f10c4337014e14d
Mahesh <msghutukade1@gmail.com>
2026-06-02 21:23:20 +0530

    Resolved POST method

--- microservice/app/main.py
+++ microservice/app/main.py
@@ -1,25 +1,48 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection 
+from redis_om import HashModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:3000'],
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
 
 redis = get_redis_connection(
     host="horse-color-cook-49943.db.redis.io",
     port=14005,
     password="F9eo0tCdGNhYYvjPUb1deZjafliCsMJJ",
-    decode_responses=True 
+    decode_responses=True
 )
 
 
-class Product():
-    name = str
-    price = float
-    quantity = int 
+class Product(HashModel, index=True):
+    name : str
+    price : float
+    quantity : int 
     
     class Meta:
         database=redis 
     
-@app.get('/prodcuts')
+@app.get("/")
+def home():
+    return {"message": "Ecommerce Microservice Running"}
+
+@app.get("/products")
 def all():
-    return []
+    return Product.all_pks()
+
+@app.post("/products")
+def create(product: Product):
+    pk = product.save()
+    return {"pk": pk}
+
+@app.get("/test")
+def test():
+    return {"ping": redis.ping()}
 
